95d2e6b0d8656550391504b55a9a200d1749f7da
tomigm <tgmoron@gmail.com>
2020-09-11 13:15:47 -0300

    Finished first styling

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import  next  from './Mfunctions';
 
 M.AutoInit();
 
-
+/*FEATURE DISCOVERY BLOCK*/
 
 const helpBtn = document.getElementById('helpBtn');
 const nextHelpBtn = document.getElementById('nextHelp');
-
 helpBtn.addEventListener('click', function() {next(0)});
 nextHelpBtn.addEventListener('click', function() {next(1)});
 
-console.log ('hola');
-
+/*FEATURE DISCOVERY BLOCK*/
  

567541a68635ea97307db4801bd422a9aea7930c
tomigm <tgmoron@gmail.com>
2020-09-15 17:23:33 -0300

    added comments to functions

--- src/projects.js
+++ src/projects.js
@@ -18,24 +18,31 @@ const render = data => {
     list.push(data);
     console.log (list);    
 
+    // renders current project data on Projectlist
     let projects = document.getElementById('projects');
     projects.append(projectTemplate(data.projectName, data.projectDescription, data.tasks.length));
     pubsub.publish('updatedList', list);
 
+    // adds an event listener to delete icon in each project (rendered) ==> runs deleteProject()
     let delProj = document.querySelectorAll('.delProj');
     delProj.forEach(elem => elem.addEventListener('click', deleteProject))
+
+  
     
 }
 
 const deleteProject = (event) => {
-    const projectList = document.getElementById('projects')
 
+    // Selects project list
+    const projectList = document.getElementById('projects')
+    // Grabs parent from clicked delete icon ==> it gets its project-name data
     let current = event.target.closest(".row");
     let name = current.getAttribute('project-name')    
-
+    //makes a new list with all the projects EXCEPT the one clicked that matches projectName
     list = list.filter(project => project.projectName !== name ) ;
+    // publishes globally the new updated list without the deleted project
     pubsub.publish('projectDeleted', list);
-
+    // removes parent from DOM
     projectList.removeChild(current);
     console.log(list)  
    /* ;

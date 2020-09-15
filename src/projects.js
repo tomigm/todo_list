c4a2afc8efc0e5fc82ecd0d3a8d7bc679ff4cc39
tomigm <tgmoron@gmail.com>
2020-09-15 17:24:13 -0300

    code cleaned

--- src/projects.js
+++ src/projects.js
@@ -45,42 +45,12 @@ const deleteProject = (event) => {
     // removes parent from DOM
     projectList.removeChild(current);
     console.log(list)  
-   /* ;
-    
-    let parent = current.parentElement;
-    parent.parentElement.removeChild(parent);*/
+ 
 }
 
 
 return { listen, render, list, deleteProject,  }
-/*
-const render = (container) =>{
-    container.appendChild(projectTemplate);
 
-    let delProj = document.querySelectorAll('.delProj i');
-    delProj.addEventListener('click', deleteProject);
-
-    console.log('PROJECTS: want to know if a Project is added');
-    pubsub.suscribe ('projectAdded', projectAdded);
-
-}
-
-
-const projectAdded = (projectName, projectDescription, tasks) => {
-    console.log(`PROJECTS: I hear that ${name} was added`);
-    project = {projectName: projectName, projectDescription: projectDescription, task : tasks};
-    list.push(project);
-
-    pubsub.publish('projectUpdated', list);
-
-    let projectList = document.getElementById('projects');
-    projectList.innerHTML = ''
-    projects.appendChild(projectTemplate(projectName, projectDescription, tasks))
-
-}
-
-return {render, deleteProject, projectAdded}
-*/
 })();
 
 export default projects;
